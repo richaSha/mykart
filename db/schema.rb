3529f362eb82b5318810c3913e14783ff2d69312
Elrey Belmonti <elbelmonti@gmail.com>
2017-10-02 11:11:28 -0700

    add Address class

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171002175325) do
+ActiveRecord::Schema.define(version: 20171002180345) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "addresses", force: :cascade do |t|
     t.integer  "user_id"
-    t.integer  "house_no"
     t.string   "street"
     t.string   "city"
     t.string   "state"
---
+++ lib/address.rb
@@ -0,0 +1,10 @@
+class Address < ActiveRecord::Base
+  belongs_to :user
+
+  validates(:user_id {presence: true})
+  validates(:street {presence: true})
+  validates(:city {presence: true})
+  validates(:state {presence: true})
+  validates(:country {presence: true})
+  validates(:zipcode {presence: true})
+end
