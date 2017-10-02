e190be1e6797a28a320def6c45990c08537fd0a9
Elrey Belmonti <elbelmonti@gmail.com>
2017-10-02 10:56:46 -0700

    create join table for carts_users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171002172826) do
+ActiveRecord::Schema.define(version: 20171002175325) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,13 @@ ActiveRecord::Schema.define(version: 20171002172826) do
   end
 
   create_table "carts", force: :cascade do |t|
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "carts_users", force: :cascade do |t|
+    t.integer  "cart_id"
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
