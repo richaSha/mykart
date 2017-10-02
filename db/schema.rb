baaa15a2e75b734706a108d22777ed82972707b9
Elrey Belmonti <elbelmonti@gmail.com>
2017-10-02 10:30:05 -0700

    remove first_name and last+_name from user; replace with name column

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171001230633) do
+ActiveRecord::Schema.define(version: 20171002172826) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -105,10 +105,9 @@ ActiveRecord::Schema.define(version: 20171001230633) do
     t.string   "username"
     t.string   "password"
     t.string   "email"
-    t.string   "first_name"
-    t.string   "last_name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
 end
--- lib/users.rb
+++ lib/users.rb
@@ -1,3 +1,9 @@
 class User  < ActiveRecord::Base
-  
+  has_many :addresses
+  has_many :reviews
+  has_many :cards
+  has_many :orders
+
+  belongs_to :cart
+
 end
