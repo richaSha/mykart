61d699c464c2503426a124ecc2eae660231097d0
Elrey Belmonti <elbelmonti@gmail.com>
2017-10-03 09:03:04 -0700

    add route to update and delete categories

--- app.rb
+++ app.rb
@@ -43,6 +43,11 @@ get('/products') do
   erb(:products)
 end
 
+get("/category/:id") do
+  @category = Category.find(params[:id])
+  erb(:category)
+end
+
 post('/create_account') do
   session['error'] = nil
   new_user = User.new({name: params['name'], username: params['username'], password: params['password'], email: params['email'], admin: false})
@@ -77,4 +82,16 @@ post("/add_category") do
     session['error'] = new_category
     redirect 'categories'
   end
+end
+
+patch("/update/category/:id") do
+  category = Category.find(params[:id])
+  category.update({name: params['new-name']})
+  redirect "/category/#{category.id}"
+end
+
+delete("/delete/category/:id") do
+  category = Category.find(params[:id])
+  Category.where(id: category.id).destroy_all
+  redirect 'categories'
 end
